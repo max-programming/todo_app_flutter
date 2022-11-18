6eba76985d1670bb1cb22daf3d312eaca49f8aa9
Max Programming <ui.sabuwala@gmail.com>
2022-11-19 00:32:15 +0530

    Add one setting

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool smallTextMode = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +19,22 @@ class SettingsPage extends StatelessWidget {
         backgroundColor: tdBGColor,
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Text('Settings Page'),
+      body: Container(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("Small text mode"),
+              trailing: Switch(
+                value: smallTextMode,
+                onChanged: (value) {
+                  setState(() {
+                    smallTextMode = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
