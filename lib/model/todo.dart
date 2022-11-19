6f99b4e0703d6cbcccdac21fd3f04ea3ab4b9aa7
Max Programming <ui.sabuwala@gmail.com>
2022-11-19 15:51:26 +0530

    Remove todosList method

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -8,15 +8,4 @@ class Todo {
     required this.todoText,
     this.isDone = false,
   });
-
-  static List<Todo> todoList() {
-    return [
-      Todo(id: '01', todoText: 'Morning Excercise'),
-      Todo(id: '02', todoText: 'Buy Groceries'),
-      Todo(id: '03', todoText: 'Check Emails', isDone: true),
-      Todo(id: '04', todoText: 'Team Meeting', isDone: true),
-      Todo(id: '05', todoText: 'Work on mobile apps for 2 hour', isDone: true),
-      Todo(id: '06', todoText: 'Dinner with Jenny'),
-    ];
-  }
 }
